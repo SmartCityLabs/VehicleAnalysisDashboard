5597d7a1c8368ef8506e37ee1e2c4842fd29f933
Aneesha Punreddy <aneeshapunreddy@Aneeshas-MacBook-Pro.local>
2015-12-06 23:34:30 -0800

    Second Commit

--- routes/barGraph.js
+++ routes/barGraph.js
@@ -1,33 +1,44 @@
 
 var mongodb = require('mongodb');
-url='mongodb://aneeshapunreddy:aneeshapunreddy@c870.candidate.51.mongolayer.com:10870/cmpe285project'
+url='mongodb://admin:admin@c796.candidate.15.mongolayer.com:10796/cmpe280project'
 	
 var db;
-db = new mongodb.Db('cmpe285project', new mongodb.Server('c870.candidate.51.mongolayer.com', 10870, {auto_reconnect:true}), {});
+db = new mongodb.Db('cmpe280project', new mongodb.Server('c796.candidate.15.mongolayer.com', 10796, {auto_reconnect:true}), {});
 
-function createGraph(callback, name)
+
+function createGraph(callback, model)
 { 
-db = new mongodb.Db('cmpe285project', new mongodb.Server('c870.candidate.51.mongolayer.com', 10870, {auto_reconnect:true}), {});
+//The 10056 is the port!
+db = new mongodb.Db('cmpe280project', new mongodb.Server('c796.candidate.15.mongolayer.com', 10796, {auto_reconnect:true}), {});
 
 db.open(function(err, p_client) {
 	
   //Notice the USERNAME and PASSWORD!
-  db.authenticate('aneeshapunreddy', 'aneeshapunreddy', function(err) { 
+  db.authenticate('admin', 'admin', function(err) {
+   //Change error handler when going into production 
    if (err) console.log(err);
    else{
-	   var collection1 = new mongodb.Collection(db, name);	
+	   var collection1 = new mongodb.Collection(db, 'audi2015Collection');
+	   var collection2 = new mongodb.Collection(db, 'mercedes2015Collection');
+	   var collection3 = new mongodb.Collection(db, 'bmw2015Collection');
 	   var results = [];
 	   
 	   collection1.aggregate(
 				   [
-				     { $group: { _id: "$cityMileage", total_products: { $sum: 1 } } }
+				     { $group: { _id: "$fuelType", total_products: { $sum: 1 } } }
 				   ],function(err,result) {
 					   results.push(result); 
 					   
-					   collection1.aggregate(
+					   collection2.aggregate(
 							   [
-							     { $group: { _id: "$highwayMileage", total_products: { $sum: 1 } } }
+							     { $group: { _id: "$fuelType", total_products: { $sum: 1 } } }
 							   ],function(err,result) {
+								   results.push(result); 
+								   
+								   collection3.aggregate(
+										   [
+										     { $group: { _id: "$fuelType", total_products: { $sum: 1 } } }
+										   ],function(err,result) {
 											   results.push(result); 
 											   	callback(err,results);
 										   });
